aac35dfc339b2b1ce67def81ab685dfa5d6cfe34
adelynchen <30806609+adelynchen@users.noreply.github.com>
2018-08-21 00:29:13 -0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+// Navigation Menu
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function goHome() {
+	location="index.html";
+}
